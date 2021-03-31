7bca1310c23725cdff30b2dc2b30eac09735cc2b
ghchu <ghchu@iflytek.com>
2021-03-31 16:51:11 +0800

    feat(code): 添加utils包

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,1 +1,17 @@
-export const print = (msg) => `Hello ${msg}`
+/**
+ * @author GuangHui
+ * @description 入口文件
+ */
+
+export * from './browser'
+export * from './copy'
+export * from './dom'
+export * from './easings'
+export * from './file-convert'
+export * from './raf-animation'
+export * from './raf'
+export * from './scrollbar-width'
+export * from './select'
+export * from './tools'
+export * from './type-judge'
+export * from './url'
